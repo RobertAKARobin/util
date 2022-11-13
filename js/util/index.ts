b78c4794ecff99b96915fce4752face434216560
RobertAKARobin <contact@robertakarobin.com>
2022-11-13 11:22:42 -0600

    condense all util functions

--- js/util/index.ts
+++ js/util/index.ts
@@ -1,8 +1,61 @@
 import * as Type from './types.d';
 
-export * from './fromKeys';
-export * from './timer';
 export { Type };
+
+export function debounce(
+	callback: () => void,
+	time: number,
+) {
+	let timer: number = null;
+	return function() {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			callback();
+			timer = null;
+		}, time);
+	};
+}
+
+export function delay(
+	callback: () => unknown,
+	time: number,
+) {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(callback()), time);
+	});
+}
+
+/**
+ * Map each of the specified keys in an object to an array of values
+ * @param _options.assertAll - If true, throws an error if not all of the object's keys are specified
+ */
+export function fromKeys<Value>(
+	keys: Array<string>,
+	input: Record<string, Value>,
+	_options: typeof fromKeys[`options`] = fromKeys.options,
+) {
+	const options = { ...fromKeys.options, ..._options };
+
+	if (options.assertAll) {
+		const delimeter = `;`;
+		const inputKeys = Object.keys(input).sort().join(delimeter);
+		const outputKeys = keys.sort().join(delimeter);
+		if (inputKeys !== outputKeys) {
+			throw new Error(`Not all keys are used:\nInput keys: ${inputKeys}\nOutput keys: ${outputKeys}`);
+		}
+	}
+
+	return keys.map((key) => {
+		if (!input[key]) {
+			throw new Error(`'${key}' is not a valid key.`);
+		} else {
+			return input[key];
+		}
+	});
+}
+fromKeys.options = {
+	assertAll: true as boolean,
+};
 
 export function isDefinedAndSame(a: unknown, b:unknown): boolean {
 	return (
@@ -20,7 +73,6 @@ type nTimesCallback<Value> = (
 	nil: undefined,
 	index: number,
 ) => Value;
-
 export function nTimes<Value>(
 	number: number,
 	contents?:
@@ -41,6 +93,27 @@ export function nTimes<Value>(
 export function roundTo(input: number, places = 2) {
 	const factor = Math.pow(10, places);
 	return Math.round(input * factor) / factor;
+}
+
+export function sleep(time: number) {
+	return delay(() => undefined, time);
+}
+
+export function throttle(
+	callback: () => unknown,
+	time: number,
+) {
+	let timer: number = null;
+	return function() {
+		if (timer) {
+			return;
+		}
+
+		timer = setTimeout(() => {
+			callback();
+			timer = null;
+		}, time);
+	};
 }
 
 export function toArray<Value>(input: Type.OneOrMany<Value>): Array<Value> {
