50901c4a921ab6debe392d8c0a1c3c6ef214ee5e
RobertAKARobin <contact@robertakarobin.com>
2022-12-12 10:21:12 -0600

    rm old comments

--- spec/run.ts
+++ spec/run.ts
@@ -1,23 +1,3 @@
-/*
-export class SpecError extends Error {}
-
-const resultTypes = [`error`, `fail`, `pass`, `skip`] as const;
-
-const testOptionDefaults = {
-	indent: `\t` as string,
-	randomize: false as boolean,
-} as const;
-
-type TestOptions = typeof testOptionDefaults;
-
-const symbols: Record<typeof resultTypes[number], string> = {
-	error: `🟡`,
-	fail: `🔴`,
-	pass: `🟢`,
-	skip: `⚪`,
-} as const;
-*/
-
 import * as path from 'path';
 
 import $ from '.';
