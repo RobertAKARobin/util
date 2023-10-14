d71ffd53580d41c10ce460b262697ffc19d2c16c
RobertAKARobin <contact@robertakarobin.com>
2023-10-13 21:13:47 -0500

    Add emit specs

--- emit/index.spec.ts
+++ emit/index.spec.ts
@@ -1,3 +1,35 @@
 import { suite, test } from '@robertakarobin/spec';
 
-export const spec = suite(`@robertakarobin/emit`, {});
+import { Emitter } from './index.ts';
+
+export const spec = suite(`@robertakarobin/emit`,
+	{
+		args: () => {
+			Emitter.subscriptions.clear();
+			return {
+				emitter: new Emitter<number>(),
+				emitter2: new Emitter<number>(),
+			};
+		},
+	},
+
+	test(`.subscriptions`, $ => {
+		$.assert(x => x(Emitter.subscriptions.size) === 0);
+		$.assert(x => x($.args.emitter.subscriptions.size) === 0);
+		$.assert(x => x($.args.emitter2.subscriptions.size) === 0);
+	}),
+
+	test(`#subscribe`, $ => {
+		$.log(`subscription 1`);
+		$.args.emitter.subscribe();
+		$.assert(x => x(Emitter.subscriptions.size) === 1);
+		$.assert(x => x($.args.emitter.subscriptions.size) === 1);
+		$.assert(x => x($.args.emitter2.subscriptions.size) === 0);
+
+		$.log(`subscription 2`);
+		$.args.emitter2.subscribe();
+		$.assert(x => x(Emitter.subscriptions.size) === 2);
+		$.assert(x => x($.args.emitter.subscriptions.size) === 1);
+		$.assert(x => x($.args.emitter2.subscriptions.size) === 1);
+	})
+);
--- emit/index.ts
+++ emit/index.ts
@@ -1,24 +1,56 @@
-export interface CfCacheOptions<Type> { // eslint-disable-line @typescript-eslint/no-unused-vars
-	limit: number;
+export class Emitter<Type> {
+	/** A collection of all active subscriptions to all Emitters. Used to debug memory management. */
+	static readonly subscriptions = new Set<Subscription<any>>(); // eslint-disable-line @typescript-eslint/no-explicit-any
+
+	/** @see {@link EmitterCache} */
+	readonly cache: EmitterCache<Type>;
+
+	/** A collection of all active subcriptions to this Emitter. */
+	readonly subscriptions = new Set<Subscription<Type>>();
+
+	constructor(options: Partial<EmitterOptions> = {}) {
+		this.cache = new EmitterCache<Type>(options.cache ?? {});
+	}
+
+	next(value: Type): Emitter<Type> {
+		this.cache.add(value);
+		for (const subscription of this.subscriptions) {
+			subscription.onEmit?.(value);
+		}
+		return this;
+	}
+
+	subscribe(onEvent?: Subscription<Type>[`onEmit`]): Subscription<Type> {
+		const subscription = new Subscription<Type>(this, onEvent);
+		Emitter.subscriptions.add(subscription);
+		this.subscriptions.add(subscription);
+		return subscription;
+	}
+
+	unsubscribe(subscription: Subscription<Type>): void {
+		Emitter.subscriptions.delete(subscription);
+		this.subscriptions.delete(subscription);
+	}
+
+	unsubscribeAll(): void {
+		this.subscriptions.forEach(Emitter.subscriptions.delete); // eslint-disable-line @typescript-eslint/unbound-method
+		this.subscriptions.clear();
+	}
 }
 
-export const CfCacheOptionsDefault: CfCacheOptions<unknown> = {
-	limit: 1,
-};
-
 /** Encloses an array of values in reverse insertion order. */
-export class CfCache<Type> {
-	private readonly _memory: Array<Type> = [];
-
+export class EmitterCache<Type> {
 	/** The quantity of values to cache. */
 	limit: number;
 
 	get list(): Array<Type> {
-		return [...this._memory];
+		return [...this.memory];
 	}
 
-	constructor(options: Partial<CfCacheOptions<Type>> = {}) {
-		this.limit = options.limit ?? CfCacheOptionsDefault.limit;
+	private readonly memory: Array<Type> = [];
+
+	constructor(options: Partial<EmitterCacheOptions> = {}) {
+		this.limit = options.limit ?? EmitterCacheOptionsDefault.limit;
 	}
 
 	add(value: Type): void {
@@ -29,57 +61,24 @@ export class CfCache<Type> {
 		if (this.limit <= 0) {
 			return;
 		}
-		this._memory.unshift(...values);
-		this._memory.splice(this.limit);
+		this.memory.unshift(...values);
+		this.memory.splice(this.limit);
 	}
 }
 
-export interface CfEmitterOptions<Type> {
-	cache: Partial<CfCacheOptions<Type>>;
+export type EmitterCacheOptions = typeof EmitterCacheOptionsDefault;
+
+export const EmitterCacheOptionsDefault = {
+	limit: 1,
+};
+
+export interface EmitterOptions {
+	cache: Partial<EmitterCacheOptions>;
 }
 
-export class CfEmitter<Type> {
-	/** A collection of all active subscriptions to all CfEmitters. Used to debug memory management. */
-	static readonly subscriptions = new Set<CfSubscription<any>>(); // eslint-disable-line @typescript-eslint/no-explicit-any
-
-	/** @see {@link CfCache} */
-	readonly cache: CfCache<Type>;
-
-	/** A collection of all active subcriptions to this CfEmitter. */
-	readonly subscriptions = new Set<CfSubscription<Type>>();
-
-	constructor(options: Partial<CfEmitterOptions<Type>> = {}) {
-		this.cache = new CfCache<Type>(options.cache ?? {});
-	}
-
-	next(value: Type): void {
-		this.cache.add(value);
-		for (const subscription of this.subscriptions) {
-			subscription.onEmit?.(value);
-		}
-	}
-
-	subscribe(onEvent?: CfSubscription<Type>[`onEmit`]): CfSubscription<Type> {
-		const subscription = new CfSubscription<Type>(this, onEvent);
-		CfEmitter.subscriptions.add(subscription);
-		this.subscriptions.add(subscription);
-		return subscription;
-	}
-
-	unsubscribe(subscription: CfSubscription<Type>): void {
-		CfEmitter.subscriptions.delete(subscription);
-		this.subscriptions.delete(subscription);
-	}
-
-	unsubscribeAll(): void {
-		this.subscriptions.forEach(CfEmitter.subscriptions.delete); // eslint-disable-line @typescript-eslint/unbound-method
-		this.subscriptions.clear();
-	}
-}
-
-export class CfSubscription<Type = unknown> {
+export class Subscription<Type> {
 	constructor(
-		readonly emitter: CfEmitter<Type>,
+		readonly emitter: Emitter<Type>,
 		public onEmit?: (value: Type) => unknown,
 	) {}
 
