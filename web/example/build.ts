df280dfba661508f7a713c841b6486bb8712da46
RobertAKARobin <contact@robertakarobin.com>
2023-11-01 00:56:28 -0500

    Add css cleaner

--- web/example/build.ts
+++ web/example/build.ts
@@ -1,3 +1,5 @@
+import CleanCSS from 'clean-css';
+
 import { build } from '@robertakarobin/web/build.ts';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -7,11 +9,14 @@ import { resolve, routes } from './routes.ts';
 const baseDir = path.join(path.dirname(fileURLToPath(import.meta.url)));
 const distDir = path.join(baseDir, `dist`);
 
+const cleanCss = new CleanCSS({ format: `beautify` });
+const styles = cleanCss.minify((await import(`./styles.css.ts`)).default).styles;
+
 build(routes, resolve, {
 	baseDir,
 	distDir,
 	statics: [
-		[(await import(`./styles.css.ts`)).default, `styles.css`],
+		[styles, `styles.css`],
 		`script.ts`,
 	],
 });
