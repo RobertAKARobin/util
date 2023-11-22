cfed89e766cba7a7e2619d49fcbb94f3386b639c
RobertAKARobin <contact@robertakarobin.com>
2023-11-22 09:30:53 -0600

    Change spec colors again

--- spec/diff.ts
+++ spec/diff.ts
@@ -1,5 +1,34 @@
 import * as Diff from 'diff';
 
+function toColors<ColorMap extends Record<string, number>>(
+	input: ColorMap
+) {
+	const out = {} as Record<keyof ColorMap, string>;
+	for (const key in input) {
+		out[key as keyof ColorMap] = `\x1b[${input[key]}m`;
+	}
+	return out;
+}
+
+const c = toColors({
+	bgBlue: 44,
+	bgBlueLight: 104,
+	bgCyan: 46,
+	bgCyanLight: 106,
+	bgGrayDark: 100,
+	bgGrayLight: 47,
+	bgGreen: 42,
+	bgGreenLight: 102,
+	bgMagenta: 45,
+	bgMagentaLight: 105,
+	bgRed: 41,
+	bgRedLight: 101,
+	dim: 2,
+	fgBlack: 30,
+	fgWhite: 97,
+	reset: 0,
+});
+
 export const diff = (
 	expected: string,
 	actual: string,
@@ -12,11 +41,11 @@ export const diff = (
 	return diff.map(line => {
 		const result = line.value;
 
-		// Using cyan and magenta for added/removed so that it doesn't conflict with the typical passed/failed colors
+		// Not using green/red for added/removed so that it doesn't conflict with the typical passed/failed colors
 		return line.added === true
-			? `\x1b[45m${result}\x1b[0m`
+			? `${c.bgGrayLight}${result}${c.reset}`
 			: line.removed === true
-				? `\x1b[46m${result}\x1b[0m`
+				? `${c.bgGrayDark}${result}${c.reset}`
 				: result;
 	}).join(``);
 };
