04cc3abd31f8856a3a16eb7709cb67df90c6c426
RobertAKARobin <contact@robertakarobin.com>
2023-11-26 17:12:07 -0600

    Add .omit to jsutil

--- jsutil/index.ts
+++ jsutil/index.ts
@@ -111,6 +111,17 @@ export function nTimes<Value>(
 	return Array(number).fill(contents) as Array<Value>;
 }
 
+export function omit<Source, Keys extends Array<keyof Source>>(
+	source: Source,
+	...keys: Keys
+) {
+	const output = { ...source };
+	for (const key of keys) {
+		delete output[key];
+	}
+	return output as Omit<Source, Keys[number]>;
+}
+
 export async function promiseConsecutive<Value>(
 	inputs: Array<(soFar: Array<Value>, index: number) => Promise<Value>>
 ): Promise<Array<Value>> {
