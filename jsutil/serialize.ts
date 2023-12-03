3dab8b1a91136893676811b91d5827f5d8bc7d2d
RobertAKARobin <contact@robertakarobin.com>
2023-12-03 15:48:37 -0600

    Handle stringable objects in serializer

--- jsutil/serialize.ts
+++ jsutil/serialize.ts
@@ -1,20 +1,27 @@
 /**
  * Serialize an object as a native JS value so that it can be included in `[on*]` attributes. TODO2: Use JSON5 or something robust
  */
-export function serialize(input: any): string { // eslint-disable-line @typescript-eslint/no-explicit-any
+export function serialize(input: unknown): string { // eslint-disable-line @typescript-eslint/no-explicit-any
 	if (input === null || input === undefined) {
 		return ``;
 	}
 	if (Array.isArray(input)) {
 		return `[${input.map(serialize).join(`,`)}]`;
 	}
+	if (typeof input === `function`) {
+		return ``;
+	}
 	if (typeof input === `object`) {
-		let out = ``;
-		for (const property in input) {
-			const value = input[property] as Record<string, unknown>; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
-			out += `${property.replaceAll(`"`, `&quot;`)}:${serialize(value)},`;
+		const toString = input.toString(); // eslint-disable-line @typescript-eslint/no-base-to-string
+		if (toString === `[object Object]`) {
+			let out = ``;
+			for (const property in input) {
+				const value = input[property as keyof typeof input] as Record<string, unknown>; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
+				out += `${property.replaceAll(`"`, `&quot;`)}:${serialize(value)},`;
+			}
+			return `{${out}}`;
 		}
-		return `{${out}}`;
+		return serialize(toString);
 	}
 	if (typeof input === `string`) {
 		const out = input
