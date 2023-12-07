60d47928df7c57186e787bc463ab18c5c09a9b3a
RobertAKARobin <contact@robertakarobin.com>
2023-12-07 02:08:17 -0600

    fix reinstall script

--- bin/reinstall.sh
+++ bin/reinstall.sh
@@ -1,11 +1,13 @@
 source "$(dirname "$0")/_repos.sh"
 
+root=$(pwd)
+
 for repo in ${repos[@]}; do
 	cd $repo
 	echo ">>> $repo"
 	rm -rf node_modules
 	npm i
-	cd ..
+	cd $root
 done
 
 rm -rf node_modules
