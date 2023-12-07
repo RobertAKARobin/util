2a757c8304067d948b19feed27f0eee01daeebc1
RobertAKARobin <contact@robertakarobin.com>
2023-12-06 23:56:08 -0600

    consolidate web

--- web/build.ts
+++ web/build.ts
@@ -9,10 +9,8 @@ import { marked } from 'marked';
 import path from 'path';
 import { serialize } from '@robertakarobin/jsutil/serialize.ts';
 
-import { Component, globals } from './component.ts';
+import { Component, globals, type Page } from './component.ts';
 import { hasExtension, type Resolver, type Router } from './router.ts';
-import { defaultLayout } from './layout.ts';
-import { type Page } from './page.ts';
 
 export type LayoutArgs = {
 	Component: typeof Component;
@@ -372,3 +370,55 @@ export class Builder {
 		void retryPort();
 	}
 }
+
+/**
+ * The default layout used to render static HTML files for SSG routes
+ */
+export const defaultLayout = (input: LayoutArgs) => `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>${input.page.value.title}</title>
+		<base href="${input.baseHref}">
+
+		${typeof input.meta === `string`
+			?	input.meta
+			: `<meta name="viewport" content="width=device-width, initial-scale=1">`
+		}
+
+		${typeof input.mainJsPath === `string`
+			? `<script src="${input.mainJsPath}" type="module"></script>`
+			: ``
+		}
+
+		${typeof input.mainCssPath === `string`
+			? `<link rel="stylesheet" href="${input.mainCssPath}">`
+			: ``
+		}
+
+		${typeof input.head === `string`
+			? input.head
+			: ``
+		}
+
+		${typeof input.routeCssPath === `string`
+			? `<link rel="stylesheet" href="${input.routeCssPath}">`
+			: ``
+		}
+
+		${typeof input.loadScript === `string`
+			? `<script>${input.loadScript}</script>`
+			: ``
+		}
+
+		${[...input.Component.subclasses.values()].map(Subclass => /* These keep Component.ts from loading the CSS twice; see Componet.setStyle */`
+			<style ${Subclass.$elAttrType}="${Subclass.name}"></style>
+		`).join(``)}
+	</head>
+
+	${typeof input.body === `string`
+		? `<body>${input.body}</body>`
+		: ``
+	}
+</html>
+`;
--- web/component.ts
+++ web/component.ts
@@ -307,3 +307,20 @@ export class Component<State = Record<string, unknown>> extends Emitter<State> {
 		return content ?? ``;
 	}
 }
+
+type PageType = {
+	title: string;
+};
+
+export abstract class Page<
+	State extends Record<string, unknown> = Record<string, unknown>,
+> extends Component<PageType & State> {
+	set(...[update, ...args]: Parameters<Component<{ title: string; } & State>[`set`]>) {
+		if (`title` in update) {
+			if (appContext === `browser`) {
+				document.title = update.title!;
+			}
+		}
+		return super.set(update, ...args);
+	}
+}
