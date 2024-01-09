2ccac7c4a1a79b7f882d1c215a36167fa576fd61
RobertAKARobin <contact@robertakarobin.com>
2024-01-09 10:10:59 -0600

    Add apiFactory

---
+++ api.ts
@@ -0,0 +1,73 @@
+export const contentTypes = {
+	json: `application/json`,
+	plain: `text/plain`,
+	urlencoded: `application/x-www-form-urlencoded`,
+} as const;
+
+export type RequestOptions<
+	Flags extends Record<string, unknown> = Record<string, never>,
+> = {
+	at: string;
+	body?: Record<string, string | number | boolean>;
+	flags?: Flags;
+	format?: keyof typeof contentTypes;
+	headers?: RequestInit[`headers`];
+	method?: `GET` | `POST` | `PATCH` | `PUT` | `DELETE`;
+	query?: Record<string, string | number | boolean>;
+};
+
+export function apiFactory<
+	Flags extends Record<string, unknown> = Record<string, never>,
+>(
+	baseUrl: string,
+	meta: {
+		flags?: Flags;
+		preprocess?: (
+			parameters: RequestInit,
+			options: RequestOptions<Flags>
+		) => void;
+	} = {}
+) {
+	return async function request<Response>(
+		options: RequestOptions<Flags>
+	) {
+		const parameters = {} as RequestInit;
+
+		let url = `${baseUrl}${options.at}`;
+		if (options.query !== undefined) {
+			const queryParams = new URLSearchParams(options.query as Record<string, string>);
+			url += `?${queryParams}`;
+		}
+
+		parameters.method = options.method ?? (
+			options.body === undefined ? `GET` : `POST`
+		);
+
+		const format = options.format ?? `plain`;
+
+		parameters.headers = new Headers();
+		parameters.headers.set(`Content-Type`, contentTypes[format]);
+
+		if (options.body !== undefined) {
+			if (format === `json`) {
+				parameters.body = JSON.stringify(options.body);
+			} else if (format === `urlencoded`) {
+				parameters.body = new URLSearchParams(options.body as Record<string, string>);
+			}
+		}
+
+		if (meta.preprocess) {
+			meta.preprocess(parameters, {
+				...options,
+				flags: {
+					...(meta.flags ?? {}),
+					...(options.flags ?? {}),
+				} as Flags,
+			});
+		}
+
+		const request = fetch(url, parameters);
+		const response = await request;
+		return await response.json() as Response;
+	};
+}
