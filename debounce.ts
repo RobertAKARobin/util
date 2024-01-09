db2f7d54fab125293a67b32c6dbd71b245598e67
RobertAKARobin <contact@robertakarobin.com>
2024-01-09 11:38:52 -0600

    Add args to debounce

--- debounce.ts
+++ debounce.ts
@@ -1,17 +1,17 @@
 import type * as Type from './types.d.ts';
 
-export function debounce(
-	callback: () => void,
-	time: number,
+export function debounce<Input>(
+	callback: (input: Input) => void,
+	delay: number,
 ) {
 	let timer: Type.Timer | null = null;
-	return function() {
+	return function(input: Input) {
 		if (timer) {
 			clearTimeout(timer);
 		}
 		timer = setTimeout(() => {
-			callback();
+			callback(input);
 			timer = null;
-		}, time);
+		}, delay);
 	};
 }
