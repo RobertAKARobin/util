d73c3d7001559740011eaa85a45bf8b88c2aea18
RobertAKARobin <contact@robertakarobin.com>
2024-01-11 08:25:35 -0600

    Updating csslint to use stylelistic-plugin

--- csslint/index.js
+++ csslint/index.js
@@ -1,7 +1,6 @@
 module.exports = {
 	extends: [
-		`stylelint-config-standard`,
-		`stylelint-stylistic/config`,
+		`@stylistic/stylelint-config`,
 	],
 	overrides: [
 		{
@@ -12,10 +11,16 @@ module.exports = {
 		},
 	],
 	plugins: [
+		`@stylistic/stylelint-plugin`,
 		`stylelint-order`,
-		`stylelint-stylistic`,
 	],
 	rules: {
+		'@stylistic/color-hex-case': `lower`,
+		'@stylistic/indentation': `tab`,
+		'@stylistic/max-empty-lines': null,
+		'@stylistic/max-line-length': null,
+		'@stylistic/number-leading-zero': `always`,
+		'@stylistic/string-quotes': `single`,
 		'alpha-value-notation': `number`,
 		'at-rule-empty-line-before': null,
 		'color-hex-length': `long`,
@@ -26,17 +31,11 @@ module.exports = {
 		'font-family-name-quotes': `always-unless-keyword`,
 		'font-family-no-missing-generic-family-keyword': null,
 		'keyframes-name-pattern': null,
-		'max-empty-lines': null,
-		'max-line-length': null,
 		'no-descending-specificity': null,
 		'order/properties-alphabetical-order': true,
 		'property-no-vendor-prefix': null,
 		'selector-class-pattern': /^(?:[-_]?[a-zA-Z0-9]+){1,}$/,
 		'selector-pseudo-class-no-unknown': null,
 		'shorthand-property-no-redundant-values': null,
-		'stylistic/color-hex-case': `lower`,
-		'stylistic/indentation': `tab`,
-		'stylistic/number-leading-zero': `always`,
-		'stylistic/string-quotes': `single`,
 	},
 };
