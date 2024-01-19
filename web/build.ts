d937c6a17a7bc7c6e1bad8ca84a3d0a00de1c575
RobertAKARobin <contact@robertakarobin.com>
2024-01-19 00:12:47 -0600

    Add meta description

--- web/build.ts
+++ web/build.ts
@@ -329,6 +329,7 @@ export class Builder {
 		routeCssPath: string;
 	}> = {}) {
 		return /*html*/`
+		<meta name="description" content="${document.title}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
