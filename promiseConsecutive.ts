28ffc2c232cece46b92a1599834f630345c12032
RobertAKARobin <contact@robertakarobin.com>
2024-02-09 13:47:27 -0600

    Add note about promiseConsecutive

--- promiseConsecutive.ts
+++ promiseConsecutive.ts
@@ -1,5 +1,9 @@
+/**
+ * Like Promise.all, except it runs the Promises in sequential order.
+ * Each Promise is wrapped in a callback; otherwise they would all start at the same time.
+ */
 export async function promiseConsecutive<Value>(
-	inputs: Array<(soFar: Array<Value>, index: number) => Promise<Value>>
+	inputs: Array<(soFar: Array<Value>, index: number) => Promise<Value>> // Using a spread ... seems to break the typing
 ): Promise<Array<Value>> {
 	const out: Array<Value> = [];
 	// TODO3: Use `for await..of` instead?
