231ce552488277a66f654d8746fc43de15d249f9
RobertAKARobin <contact@robertakarobin.com>
2024-02-16 10:42:45 -0600

    Include minified in dist

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,10 @@
+utils=(components delimiter-pairs emitter math spec .)
+
 rm -rf dist
-esbuild --outdir=dist *.ts
+
+for util in ${utils[@]}; do
+	esbuild --outdir=dist/$util $util/*.ts
+	esbuild --outdir=dist/$util --minify --sourcemap --entry-names="[dir]/[name].min" $util/*.ts
+done
 tsc
 cp types.d.ts dist
