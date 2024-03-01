9ae6d4dec9fb678a7f00fa13941deaead1e8e707
RobertAKARobin <contact@robertakarobin.com>
2024-03-01 11:13:16 -0600

    alpha types

--- util/types.d.ts
+++ util/types.d.ts
@@ -6,24 +6,24 @@ export type KeysMatching<Type, Value> = { // https://stackoverflow.com/q/7757188
 	[Key in keyof Type]: Type[Key] extends Value ? Key : never
 }[keyof Type];
 
-export type OneOrMany<Type> = Array<Type> | Type;
+export type Index1<Input> =
+	Input extends [param1: infer Param, ...rest: any] // eslint-disable-line @typescript-eslint/no-explicit-any
+		? Param
+		: never;
+
+export type Index1Forward<Input> =
+	Input extends [param1: any, ...rest: infer Rest] // eslint-disable-line @typescript-eslint/no-explicit-any
+		? Rest
+		: never;
 
 export type Nested<Type> = Array<Nested<Type> | Type>;
+
+export type OneOrMany<Type> = Array<Type> | Type;
 
 export type PromiseMaybe<Type> = Promise<Type> | Type;
 
 export type RequireOnly<_Object, _RequiredKeys extends keyof _Object> = Partial<_Object>
 	& Pick<_Object, _RequiredKeys>;
-
-export type FromIndex1<Input> =
-	Input extends [param1: any, ...rest: infer Rest] // eslint-disable-line @typescript-eslint/no-explicit-any
-		? Rest
-		: never;
-
-export type AtIndex1<Input> =
-	Input extends [param1: infer Param, ...rest: any] // eslint-disable-line @typescript-eslint/no-explicit-any
-		? Param
-		: never;
 
 export type Textish = URL | boolean | number | string | symbol | null | undefined;
 
