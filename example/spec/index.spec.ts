615921f7a4c6cb673952e75cefc0e13545ae0362
RobertAKARobin <contact@robertakarobin.com>
2024-03-21 12:32:14 -0500

    rm unused spec

--- example/spec/index.spec.ts
+++ example/spec/index.spec.ts
@@ -1,5 +1,53 @@
-import { suite } from '@robertakarobin/util/spec/index.ts';;
+import '@robertakarobin/util/dom/dummydom.ts';
 
-export const spec = suite(`@robertakarobin/js`, {},
-	(await import(`./build.spec.ts`)).spec,
+import { suite, test } from '@robertakarobin/util/spec/index.ts';
+import { Component } from '@robertakarobin/util/components/component.ts';
+import { diff } from '@robertakarobin/util/spec/diff.ts';
+import { EntityStateEmitter } from '@robertakarobin/util/emitter/entities.ts';
+
+import fs from 'fs';
+
+const uid = () => `UID${++id}_`; // Easy to strip out with RegEx
+
+let id = 0;
+EntityStateEmitter.prototype.createId = uid;
+Object.assign(Component, { uid });
+
+export const hasMarkdown = /<markdown>(.*?)<\/markdown>/gs;
+
+const read = (path: string) => fs.readFileSync(path, { encoding: `utf8` })
+	.replace(/UID\d+_/ig, `UID`) // Note that this will make it look like some IDs are repeated
+	.replace(/\?cache=\d+/g, `?cache=123`)
+	.replace(/-[A-Z0-9]{8}\.js/g, `.js`);
+
+const dist = (path: string) => read(`dist/${path}`);
+const golden = (path: string) => read(`dist-golden/${path}`);
+const src = (path: string) => read(`src/${path}`);
+
+const distMatchesGolden = (path: string) =>
+	diff(golden(path), dist(path));
+
+const hasSSG = (page: string) =>
+	fs.existsSync(`dist/${page}.html`);
+
+export const spec = suite(`Build`, {},
+	test(`build`, async $ => {
+		await import(`../build.ts`);
+
+		$.assert(x => x(distMatchesGolden(`ssg/yes/index.html`)) === ``);
+		$.assert(x => x(distMatchesGolden(`ssg/yes/index.html.css`)) === ``);
+		$.assert(x => x(distMatchesGolden(`404.html`)) === ``);
+		$.assert(x => x(distMatchesGolden(`404.html.css`)) === ``);
+		$.assert(x => x(distMatchesGolden(`index.html`)) === ``);
+		$.assert(x => x(distMatchesGolden(`index.html.css`)) === ``);
+		$.assert(x => x(distMatchesGolden(`styles.css`)) === ``);
+
+		$.assert(() => hasSSG(`404`));
+		$.assert(() => hasSSG(`index`));
+		$.assert(() => hasSSG(`ssg/yes/index`));
+		$.assert(() => !hasSSG(`ssg/no/index`));
+
+		$.assert(() => hasMarkdown.test(src(`pages/index.ts`)));
+		$.assert(() => !hasMarkdown.test(dist(`index.html`)));
+	}),
 );
