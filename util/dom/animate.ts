d20a1486a43bcc3a7b81bfda947c770b0526d479
RobertAKARobin <contact@robertakarobin.com>
2024-04-08 21:46:43 -0500

    Dont set initialState

--- util/dom/animate.ts
+++ util/dom/animate.ts
@@ -15,7 +15,6 @@ export async function animate(
 			}
 		}, { once: true });
 
-		target.setAttribute(`style`, target.getAttribute(`style`) + `;` + input.initialState);
 		target.style.setProperty(`animation-duration`, `${input.timeDuration}s`);
 		target.style.setProperty(`animation-fill-mode`, `forwards`);
 		target.style.setProperty(`animation-name`, input.name as string);
