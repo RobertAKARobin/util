fadb6bc5164838b6d1408b74f3a642c44c32c041
RobertAKARobin <contact@robertakarobin.com>
2024-05-07 09:18:26 -0500

    Add pointNearestPoint specs

--- util/math/pointNearestPoint.spec.ts
+++ util/math/pointNearestPoint.spec.ts
@@ -1,7 +1,21 @@
 import { test } from '../spec/index.ts';
 
+import type { CoordinateLike } from '../types.d.ts';
 import { pointNearestPoint } from './pointNearestPoint.ts';
+import { pointToString } from './pointToString.ts';
 
 export const spec = test(`pointNearestPoint`, $ => {
+	const points = [[0, 0], [3, 0], [3, 4]];
+	const result = (target: CoordinateLike) => {
+		const result = pointNearestPoint(target, ...points);
+		return pointToString(result);
+	};
 
+	$.assert(x => x(result([0, 0])) === `0,0`);
+	$.assert(x => x(result([1, 1])) === `0,0`);
+
+	$.assert(x => x(result([3, 0])) === `3,0`);
+	$.assert(x => x(result([3, 1])) === `3,0`);
+
+	$.assert(x => x(result([2, 4])) === `3,4`);
 });
