36d2b46cb68df5ce96ce24adedb4ab224091d59d
RobertAKARobin <contact@robertakarobin.com>
2024-05-08 21:47:10 -0500

    nTimes iterates over indexes

--- util/group/nTimes.spec.ts
+++ util/group/nTimes.spec.ts
@@ -5,4 +5,5 @@ import { nTimes } from './nTimes.ts';
 export const spec = test(`nTimes`, $ => {
 	$.assert(x => x(nTimes(2, `foo`).join(` `)) === `foo foo`);
 	$.assert(x => x(nTimes(3, (nil, index) => index).join(` `)) === `0 1 2`);
+	$.assert(x => x(nTimes(3).join(` `)) === `0 1 2`);
 });
--- util/group/nTimes.ts
+++ util/group/nTimes.ts
@@ -6,14 +6,14 @@ type nTimesCallback<Value> = (
 /**
  * Perform the given operation n times and returns the result as an array
  */
-export function nTimes<Value>(
+export function nTimes<Value = number>(
 	number: number,
 	contents?:
 		| nTimesCallback<Value>
 		| Value,
 ): Array<Value> {
 	if (contents === null || contents === undefined) {
-		return Array.from(Array(number)) as Array<Value>;
+		return Array.from(Array(number), (_nil, index) => index) as Array<Value>;
 	}
 	if (typeof contents === `function`) {
 		return Array.from(Array(number), contents as nTimesCallback<Value>);
