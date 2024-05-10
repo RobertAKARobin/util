7adbdff4fcf7dd732eb30ad673dd3db1b594d293
RobertAKARobin <contact@robertakarobin.com>
2024-05-09 21:45:43 -0500

    toCoordinate accepts strings

--- util/math/toCoordinate.spec.ts
+++ util/math/toCoordinate.spec.ts
@@ -3,14 +3,18 @@ import { test } from '../spec/index.ts';
 import { toCoordinate } from './toCoordinate.ts';
 
 export const spec = test(import.meta.url, $ => {
-	$.assert(x => x(toCoordinate([3, 6])).x === 3);
-	$.assert(x => x(toCoordinate([3, 6])).y === 6);
-	$.assert(x => x(toCoordinate([3, 6, 9])).x === 3);
-	$.assert(x => x(toCoordinate([3, 6, 9])).y === 6);
-	$.assert(x => x(toCoordinate([3, -6])).x === 3);
-	$.assert(x => x(toCoordinate([3, -6])).y === -6);
-	$.assert(x => x(toCoordinate([-3, -6])).x === -3);
-	$.assert(x => x(toCoordinate([-3, -6])).y === -6);
-	$.assert(x => x(toCoordinate({ x: 3, y: 6 })).x === 3);
-	$.assert(x => x(toCoordinate({ x: 3, y: 6 })).y === 6);
+	$.assert(x => x(toCoordinate([3, 6]).x) === 3);
+	$.assert(x => x(toCoordinate([3, 6]).y) === 6);
+	$.assert(x => x(toCoordinate([3, 6, 9]).x) === 3);
+	$.assert(x => x(toCoordinate([3, 6, 9]).y) === 6);
+	$.assert(x => x(toCoordinate([3, -6]).x) === 3);
+	$.assert(x => x(toCoordinate([3, -6]).y) === -6);
+	$.assert(x => x(toCoordinate([-3, -6]).x) === -3);
+	$.assert(x => x(toCoordinate([-3, -6]).y) === -6);
+	$.assert(x => x(toCoordinate({ x: 3, y: 6 }).x) === 3);
+	$.assert(x => x(toCoordinate({ x: 3, y: 6 }).y) === 6);
+	$.assert(x => x(toCoordinate(`3,6`).x) === 3);
+	$.assert(x => x(toCoordinate(`3,6`).y) === 6);
+	$.assert(x => x(toCoordinate(`4  7`).x) === 4);
+	$.assert(x => x(toCoordinate(`4  7`).y) === 7);
 });
--- util/math/toCoordinate.ts
+++ util/math/toCoordinate.ts
@@ -3,7 +3,11 @@ import type { Coordinate, CoordinateLike } from '../types.d.ts';
 /**
  * Converts a tuple of 2 numbers to a Coordinate. Using `Array<number>` because otherwise keeps requiring we specify `as [number, number]`
  */
-export function toCoordinate(coordinateLike: CoordinateLike): Coordinate {
+export function toCoordinate(coordinateLike: CoordinateLike | string): Coordinate {
+	if (typeof coordinateLike === `string`) {
+		const [x, y] = coordinateLike.split(/[, ]+/).map(Number);
+		return { x, y };
+	}
 	if (Array.isArray(coordinateLike)) {
 		return {
 			x: coordinateLike[0],
