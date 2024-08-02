474af0b5f67d6c12dc43871994f12904434288f6
RobertAKARobin <contact@robertakarobin.com>
2024-08-02 12:54:56 -0500

    Add tests for resetting args

--- util/spec/spec.spec.ts
+++ util/spec/spec.spec.ts
@@ -1,11 +1,12 @@
 import { render, suite, test } from './index.ts';
 import { diff } from './diff.ts';
+import { sleep } from '../time/sleep.ts';
 
 import * as dbTests from './example/db.spec.ts';
 import * as mathTests from './example/math.spec.ts';
 
 export const spec = suite(`SpecRunner`, {},
-	suite(`math`,
+	suite(`example: math`,
 		{
 			args: async() => {
 				const expected = mathTests.expected.trim();
@@ -23,7 +24,7 @@ export const spec = suite(`SpecRunner`, {},
 		}),
 	),
 
-	suite(`db`,
+	suite(`example: db`,
 		{
 			args: async() => {
 				const expected = dbTests.expected.trim();
@@ -39,5 +40,140 @@ export const spec = suite(`SpecRunner`, {},
 		test(`rendered results match expected`, ({ args, assert }) => {
 			assert(x => x(diff(args.expected, args.rendered)) === ``);
 		}),
+	),
+
+	suite(`args`,
+		{
+			args: () => ({
+				value1: `foo`,
+			}),
+		},
+
+		test(`are passed to tests`, ({ args, assert }) => {
+			assert(x => x(args.value1) === `foo`);
+		}),
+
+		...(() => {
+			const state = {
+				suite1Args: {} as unknown,
+				suite2Args: {} as unknown,
+				test1Args: {} as { value1: string; value1_1: string; },
+				test2Args: {} as { value1: string; value1_1: string; },
+			};
+
+			return [
+				suite(`are ihnerited`,
+					{
+						args: (args: { value1: string; }) => {
+							state.suite1Args = args;
+							return args;
+						},
+					},
+
+					test(`from parent suites`, ({ args, assert }) => {
+						assert(() => args === state.suite1Args);
+						assert(x => x(args.value1) === `foo`);
+					}),
+				),
+
+				suite(`are reevaluated between siblings`,
+					{
+						args: (args: { value1: string; }) => {
+							state.suite2Args = args;
+							return {
+								...args,
+								value1_1: `${args.value1}bar`,
+							};
+						},
+					},
+
+					test(`suites`, ({ args, assert }) => {
+						assert(x =>
+							x(JSON.stringify(state.suite1Args))
+							=== x(JSON.stringify(state.suite2Args)),
+						);
+						assert(() => state.suite1Args !== state.suite2Args);
+
+						state.test1Args = args;
+						args.value1 = `no longer foo`;
+						assert(x => x(args.value1_1) === `foobar`);
+					}),
+
+					test(`tests`, ({ args, assert }) => {
+						state.test2Args = args;
+
+						assert(x => x(state.test1Args.value1) === `no longer foo`);
+						assert(x => x(state.test2Args.value1) === `foo`);
+						assert(x => x(state.test2Args.value1_1) === `foobar`);
+					}),
+				),
+			];
+		})(),
+	),
+
+	suite(`suite timing`, {},
+		...(() => {
+			const state = {
+				value: `state1`,
+			};
+
+			return [
+				suite(`when concurrent`,
+					{
+						args: () => ({
+							value: `args1`,
+						}),
+						timing: `concurrent`,
+					},
+
+					test(`args are unaffected`, async({ args, assert }) => {
+						assert(x => x(state.value) === `state1`);
+						assert(x => x(args.value) === `args1`);
+
+						await sleep(100);
+
+						assert(x => x(state.value) === `state2`);
+						assert(x => x(args.value) === `args1`);
+					}),
+
+					test(`tests start at the same time`, ({ args, assert }) => {
+						assert(x => x(state.value) === `state1`);
+						assert(x => x(args.value) === `args1`);
+
+						state.value = `state2`;
+						args.value = `args2`;
+					}),
+				),
+
+				suite(`when consecutive`,
+					{
+						args: () => ({
+							value: `args1`,
+						}),
+						timing: `consecutive`,
+					},
+
+					test(`args are unaffected`, async({ args, assert }) => {
+						assert(x => x(state.value) === `state2`);
+						assert(x => x(args.value) === `args1`);
+
+						await sleep(100);
+
+						assert(x => x(state.value) === `state2`);
+						assert(x => x(args.value) === `args1`);
+
+						state.value = `state3`;
+					}),
+
+					test(`tests run in order`, ({ args, assert }) => {
+						assert(x => x(state.value) === `state3`);
+						assert(x => x(args.value) === `args1`);
+
+						state.value = `state4`;
+						args.value = `args4`;
+					}),
+				),
+			];
+		})(),
 	),
 );
