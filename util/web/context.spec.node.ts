b8bd023b82917bc9bd212e7204f0adfcb4c74cfc
RobertAKARobin <contact@robertakarobin.com>
2024-11-17 21:59:51 -0600

    Add runContext specs

---
+++ util/web/context.spec.node.ts
@@ -0,0 +1,7 @@
+import { test } from '../spec/index.ts';
+
+import { runContext } from './context.ts';
+
+export const spec = test(import.meta.url, $ => {
+	$.assert(x => x(runContext) === `server`);
+});
