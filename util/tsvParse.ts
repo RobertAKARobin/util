83538fd6e61cc8e9a13390d866a23d02a8adb6a8
RobertAKARobin <contact@robertakarobin.com>
2025-01-06 17:15:05 -0600

    Add options/doc to tsvParse

--- util/tsvParse.ts
+++ util/tsvParse.ts
@@ -1,27 +1,33 @@
+/**
+ * Converts a TSV string into an array of objects, using the given callback on each row
+ */
 export function tsvParse<Entry>(
 	input: string,
 	convert: (input: Array<string>) => Entry,
-	options: Partial<{
-		delimiter: string;
-	}> = {},
+	options: {
+		columnDelimiter?: string;
+		lineDelimiter?: string;
+	} = {},
 ) {
-	const delimiter = options.delimiter ?? `\t`;
+	const lineDelimiter = options.lineDelimiter ?? `\n`;
+	const columnDelimiter = options.columnDelimiter ?? `\t`;
 
 	const text = input.trim();
 	if (text.length === 0) {
 		return [];
 	}
 
-	const entries: Array<Entry> = [];
+	const entries = [];
 
-	for (const line of text.split(`\n`)) {
-		const columns = line.trimEnd().split(delimiter);
+	for (const line of text.split(lineDelimiter)) {
+		const columns = line.trimEnd().split(columnDelimiter);
 		if (columns.length === 0) {
 			continue;
 		}
 
-		const entry: Entry = convert(columns);
+		const entry = convert(columns);
 		entries.push(entry);
 	}
 	return entries;
 }
+
