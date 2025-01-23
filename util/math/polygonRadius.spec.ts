e6557485a9fa623c01141da4bd77dd09c93f2baf
RobertAKARobin <contact@robertakarobin.com>
2025-01-23 13:45:06 -0600

    Add polygonRadius

---
+++ util/math/polygonRadius.spec.ts
@@ -0,0 +1,28 @@
+import { test } from '../spec/index';
+
+import { preciseTo } from './preciseTo';
+
+import { polygonRadius } from './polygonRadius';
+
+function hypotenuseFromSide(sideLength: number) {
+	return preciseTo(Math.sqrt(2 * Math.pow(sideLength, 2)));
+}
+
+function sideFromHypotenuse(hypotenuseLength: number) {
+	return preciseTo(
+		Math.sqrt(
+			Math.pow(hypotenuseLength, 2) / 2,
+		),
+	);
+}
+
+export const spec = test(import.meta.url, $ => {
+	$.assert(x => x(polygonRadius(3, 3, `inner`)) === 1.5);
+	$.assert(x => x(polygonRadius(3, 3, `outer`)) === 6);
+
+	$.assert(x => x(polygonRadius(8, 3, `inner`)) === 4);
+	$.assert(x => x(polygonRadius(8, 3, `outer`)) === 16);
+
+	$.assert(x => x(polygonRadius(4, 4, `inner`)) === x(sideFromHypotenuse(4)));
+	$.assert(x => x(polygonRadius(4, 4, `outer`)) === x(hypotenuseFromSide(4)));
+});
---
+++ util/math/polygonRadius.ts
@@ -0,0 +1,21 @@
+import { preciseTo } from './preciseTo';
+
+/**
+ * Given the outer radius of a polygon return its inner radius, and vice-versa
+ */
+export function polygonRadius(
+	inputRadius: number,
+	sides: number,
+	outputType: `inner` | `outer`,
+) {
+	if (sides === Infinity) {
+		return inputRadius;
+	}
+
+	const innerAngle = (360 / sides) / 2;
+	const factor = Math.cos(innerAngle / (180 / Math.PI));
+	return preciseTo(outputType === `inner`
+		? inputRadius * factor
+		: inputRadius / factor,
+	);
+}
