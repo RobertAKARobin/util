875d8daaa8b83a04230d1f06d051fea75d2b43b7
RobertAKARobin <contact@robertakarobin.com>
2025-01-25 13:44:55 -0600

    arrayCoerce

--- util/group/arrayCoerce.js
+++ util/group/arrayCoerce.js
@@ -1,8 +1,13 @@
-import type { OneOrMany } from '../types.d';
+/**
+ * @import {OneOrMany} from '../types.d.ts'
+ */
 
 /**
  * Converts an item to an array, if it isn't one already
+ * @template Input
+ * @param {OneOrMany<Input>} input
+ * @returns {Array<Input>}
  */
-export function arrayCoerce<Value>(input: OneOrMany<Value>): Array<Value> {
+export function arrayCoerce(input) {
 	return (input instanceof Array ? input : [input]);
 }
