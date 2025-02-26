5a0048b538f2960c809f305fbe1a6419edd3de41
RobertAKARobin <contact@robertakarobin.com>
2025-02-26 14:53:54 -0600

    rm skipLibChecks

--- util/components/types.d.ts
+++ util/components/types.d.ts
@@ -5,13 +5,23 @@ export type AttributeValue =
 	| number
 	| string;
 
-export type AttributeNames<Instance, AttributeKey> =
+export type AttributeNames<Instance, AttributeKey extends keyof Instance> =
 	Instance[AttributeKey] extends AttributeValue
 	? (AttributeKey | [AttributeKey, string])
 	: never;
 
-export type EventNames<Instance, EventKey> =
+type DecoratedInstance<
+	Instance,
+	EventKey extends keyof Instance,
+	AttributeKey extends keyof Instance
+> = {
+	new():
+		& Instance
+		& { [Key in AttributeKey]: Instance[Key] & IsAttribute }
+		& { [Key in EventKey]: Instance[Key] & IsEvent };
+};
+
+export type EventNames<Instance, EventKey extends keyof Instance> =
 	Instance[EventKey] extends () => void
 	? (EventKey | [EventKey, string])
 	: never;
-
--- util/dom/types.d.ts
+++ util/dom/types.d.ts
@@ -11,4 +11,4 @@ declare global {
 	interface GlobalEventHandlersEventMap { // eslint-disable-line no-restricted-syntax
 		customdrag: CustomDragEvent;
 	}
-};
+}
