e9f27f3745fe234820c0ca521567255561ecc074
RobertAKARobin <contact@robertakarobin.com>
2025-03-28 15:22:51 -0500

    flesh out split tests

--- util/string/split.js
+++ util/string/split.js
@@ -1,31 +1,31 @@
 /**
- * Split a string using a callback function
+ * Split a string using a callback function. This is useful when you want to split a string on a delimiter but include the delimiter in the resulting chunks.
  * @param {string} subject
- * @param {(index: number) => void | [number, number]} callback - When you want to split off a new chunk, return `[delimiterStartIndex, delimiterLength]`
+ * @param {(index: number, subject: string) => void | number} callback - When you want to split off a new chunk, return `delimiterLength`, which is how many characters after `index` should be excluded before the next chunk begins
  * @returns {Array<string>}
  */
 export function split(subject, callback) {
 	const results = /** @type {Array<string>} */([]);
 
 	const length = subject.length;
+	let index = 0;
 	let chunkStartIndex = 0;
-	for (let index = 0; index < length; index += 1) {
-		const delimiter = callback(index);
+	while (index < length) {
+		const delimiterLength = callback(index, subject);
 
-		if (delimiter === undefined) {
+		if (delimiterLength === undefined) {
+			index += 1;
 			continue;
 		}
 
-		const [delimiterStartIndex, delimiterLength] = delimiter;
-
-		const chunk = subject.slice(chunkStartIndex, delimiterStartIndex);
+		const chunk = subject.slice(chunkStartIndex, index);
 		results.push(chunk);
 
-		index = delimiterStartIndex + delimiterLength;
-		chunkStartIndex = index;
+		chunkStartIndex = index + delimiterLength;
+		index += Math.max(1, delimiterLength);
 	}
 
-	if (chunkStartIndex < length) {
+	if (chunkStartIndex <= length) {
 		const chunk = subject.slice(chunkStartIndex);
 		results.push(chunk);
 	}
--- util/string/split.spec.js
+++ util/string/split.spec.js
@@ -1,20 +1,88 @@
-import { test } from '../spec/index.js';
+import { suite, test } from '../spec/index.js';
 
 import { split } from './split.js';
 
-export const spec = test(import.meta.url, $ => {
-	const subject = `aa bb cc`;
-	const result = split(subject, index => {
-		const nextCharacter = subject[index];
-		if (nextCharacter === ` `) {
-			return [index, 1];
-		}
+export const spec = suite(import.meta.url, {},
+	test(`split is same as standard String.split`, $ => {
+		const subject = `aa</>bb</></>cc</></></>`;
 
-		return void 0;
-	});
+		const result = split(subject, (index, subject) =>
+			subject.substring(index).startsWith(`</>`) ? 3 : void 0,
+		);
 
-	$.assert(x => x(result.length) === 3);
-	$.assert(x => x(result[0]) === `aa`);
-	$.assert(x => x(result[1]) === `bb`);
-	$.assert(x => x(result[2]) === `cc`);
-});
+		const standard = subject.split(`</>`);
+		$.assert(x => x(result.length) === x(standard.length));
+		$.assert(x => x(result.length) === 7);
+		$.assert(x => x(result.join(`%`)) === x(standard.join(`%`)));
+
+		$.assert(x => x(result[0]) === `aa`);
+		$.assert(x => x(result[1]) === `bb`);
+		$.assert(x => x(result[2]) === ``);
+		$.assert(x => x(result[3]) === `cc`);
+		$.assert(x => x(result[4]) === ``);
+		$.assert(x => x(result[5]) === ``);
+		$.assert(x => x(result[6]) === ``);
+	}),
+
+	test(`split before delimiter`, $ => {
+		const result = split(`aa</>bb</></>cc</></></>`, (index, subject) =>
+			subject.substring(index).startsWith(`</>`) ? 0 : void 0,
+		);
+
+		$.assert(x => x(result.length) === 7);
+		$.assert(x => x(result[0]) === `aa`);
+		$.assert(x => x(result[1]) === `</>bb`);
+		$.assert(x => x(result[2]) === `</>`);
+		$.assert(x => x(result[3]) === `</>cc`);
+		$.assert(x => x(result[4]) === `</>`);
+		$.assert(x => x(result[5]) === `</>`);
+		$.assert(x => x(result[6]) === `</>`);
+	}),
+
+	test(`split after delimiter`, $ => {
+		const result = split(`aa</>bb</></>cc</></></>`, (index, subject) =>
+			subject.substring(index - 3, index) === `</>` ? 0 : void 0,
+		);
+
+		$.assert(x => x(result.length) === 6);
+		$.assert(x => x(result[0]) === `aa</>`);
+		$.assert(x => x(result[1]) === `bb</>`);
+		$.assert(x => x(result[2]) === `</>`);
+		$.assert(x => x(result[3]) === `cc</>`);
+		$.assert(x => x(result[4]) === `</>`);
+		$.assert(x => x(result[5]) === `</>`);
+	}),
+
+	test(`start with delimiter`, $ => {
+		const result = split(`</>aa</>bb</></>cc</></></>`, (index, subject) =>
+			subject.substring(index - 3, index) === `</>` ? 0 : void 0,
+		);
+
+		$.assert(x => x(result.length) === 7);
+		$.assert(x => x(result[0]) === `</>`);
+		$.assert(x => x(result[1]) === `aa</>`);
+		$.assert(x => x(result[2]) === `bb</>`);
+		$.assert(x => x(result[3]) === `</>`);
+		$.assert(x => x(result[4]) === `cc</>`);
+		$.assert(x => x(result[5]) === `</>`);
+		$.assert(x => x(result[6]) === `</>`);
+	}),
+
+	test(`only delimiter`, $ => {
+		const result = split(`</>`, (index, subject) =>
+			subject.substring(index - 3, index) === `</>` ? 0 : void 0,
+		);
+
+		$.assert(x => x(result.length) === 1);
+		$.assert(x => x(result[0]) === `</>`);
+	}),
+
+	test(`no delimiter match`, $ => {
+		const result = split(`abc`, (index, subject) =>
+			subject.substring(index - 3, index) === `</>` ? 0 : void 0,
+		);
+
+		$.assert(x => x(result.length) === 1);
+		$.assert(x => x(result[0]) === `abc`);
+	}),
+);
