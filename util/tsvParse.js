c074a5eeff3b6bd3d1323eae67aa5ef98a594cd1
RobertAKARobin <contact@robertakarobin.com>
2026-03-03 09:52:48 -0600

    tsvParse trimStart

--- util/tsvParse.js
+++ util/tsvParse.js
@@ -6,11 +6,13 @@
  * @param {object} [options]
  * @param {string} [options.columnDelimiter='\t']
  * @param {string} [options.lineDelimiter='\t']
+ * @param {boolean} [options.trimStart=false]
  * @returns {Array<Entry>}
  */
 export function tsvParse(input, convert, options = {}) {
-	const lineDelimiter = options.lineDelimiter ?? `\n`;
 	const columnDelimiter = options.columnDelimiter ?? `\t`;
+	const lineDelimiter = options.lineDelimiter ?? `\n`;
+	const trimStart = options.trimStart ?? false;
 
 	const text = input.trim();
 	if (text.length === 0) {
@@ -20,7 +22,9 @@ export function tsvParse(input, convert, options = {}) {
 	const entries = [];
 
 	for (const line of text.split(lineDelimiter)) {
-		const columns = line.trimEnd().split(columnDelimiter);
+		const columns = (
+			trimStart ? line.trim() : line.trimEnd()
+		).split(columnDelimiter);
 		if (columns.length === 0) {
 			continue;
 		}
