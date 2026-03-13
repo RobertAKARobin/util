0d977b4b3fcaddf3c4fe1f6be037980a1786c56a
RobertAKARobin <contact@robertakarobin.com>
2026-03-13 15:15:56 -0500

    note about requestAnimationFrame

--- util/time/fpsLoop.js
+++ util/time/fpsLoop.js
@@ -18,7 +18,8 @@ const msPerSecond = 1000;
 
 /**
  * Loops over the given callback at the given number of iterations per second.
- * TODO1: Rename to Loop
+ * Note: this uses `requestAnimationFrame` in the browser, which has a rate of about 16ms in an active tab and may stop entirely in an inactive tab.
+ * TODO1: Option to use web worker with setInterval?
  */
 export class FPSLoop extends Timer {
 	/**
