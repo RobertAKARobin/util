d35d5f3be13ee49f8779ba6b32c260a6da68cdef
RobertAKARobin <contact@robertakarobin.com>
2026-08-21 23:11:03 -0500

    member-orderng specs

--- eslint-config-ts/rules/member-ordering.test.js
+++ eslint-config-ts/rules/member-ordering.test.js
@@ -1,42 +1,115 @@
 import { RuleTester } from '@typescript-eslint/rule-tester';
 
-import { default as rule } from './member-ordering.js';
+import { messageId, default as rule } from './member-ordering.js';
 
 const ruleTester = new RuleTester();
 
+`class Poo {
+	aaa;
+
+	static aaa;
+
+	static ['mySignature'];
+
+	#myPrivate = 32;
+
+	_myProtected = 'foo';
+
+	get myProtected() {
+		return 'bar';
+	}
+
+	set myProtected() {
+	}
+
+	['mySignature']() {}
+	[42]() {}
+	get ['myGetSignature'] (){}
+
+	[myVariable]() {}
+
+	constructor() {}
+
+	static {}
+
+	myMethodProperty = () => {}
+}`;
+
 ruleTester.run(`member-ordering`, rule, {
 	valid: [
-		`class Poo {
-			aaa;
-
+		`class Foo {
 			static aaa;
-
-			static ['mySignature'];
-
-			#myPrivate = 32;
-
-			_myProtected = 'foo';
-
-			get myProtected() {
-				return 'bar';
-			}
-
-			set myProtected() {
-			}
-
-			['mySignature']() {}
-			[42]() {}
-			get ['myGetSignature'] (){}
-
-			[myVariable]() {}
-
-			constructor() {}
+			static _BBB;
+			static #BBB;
+			static get BBB(){}
+			static set BBB(){}
+			static zzz;
 
 			static {}
 
-			myMethodProperty = () => {}
+			static aaa(){}
+			static BBB(){}
+			static zzz(){}
+
+			aaa;
+			_BBB;
+			#BBB;
+			get BBB(){}
+			set BBB(){}
+			zzz;
+
+			constructor(){}
+
+			aaa(){}
+			BBB(){}
+			zzz(){}
 		}`,
 	],
 
-	invalid: [],
+	invalid: [
+		{
+			code: `class Foo { static zzz; static aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { zzz; static aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { get aaa(){}; #aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { get aaa(){}; _aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { set aaa(){}; get aaa(){}; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { static {}; static aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { static aaa(){}; static {}; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { constructor(){}; aaa; }`,
+			errors: [{ messageId }],
+		},
+
+		{
+			code: `class Foo { aaa(){}; constructor(){}; }`,
+			errors: [{ messageId }],
+		},
+	],
 });
